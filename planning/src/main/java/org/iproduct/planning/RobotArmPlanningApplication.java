d30867d9fde68fd0bb3b8fe808e2560fc0e84fce
Trayan Iliev <office@iproduct.org>
2019-12-03 15:11:39 +0200

    reactive Spring webflux added to project

---
+++ planning/src/main/java/org/iproduct/planning/RobotArmPlanningApplication.java
@@ -0,0 +1,13 @@
+package org.iproduct.planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotArmPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotArmPlanningApplication.class, args);
+	}
+
+}
---
+++ planning/src/test/java/org/iproduct/planning/RobotArmPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iproduct.planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotArmPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
